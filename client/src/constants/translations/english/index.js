18bd5621881965af41876f6a0daa021d2bb34239
Ahmed Shatat <a.shatat@hotmail.com>
2021-07-13 09:11:40 +0300

    store language in localstorage and create steps context
    #27

--- client/src/constants/translations/english/index.js
+++ client/src/constants/translations/english/index.js
@@ -18,10 +18,10 @@ export default {
   // STEPS
   checkEligibility: {
     title: 'Check Eligibility',
+    secondaryTitle: 'Check Eligibility',
     subtitle:
       'You can check your eligibility to claim Universal Credit with an eligibility calculator.',
     externalButtonTitle: 'Eligibility calculator',
-    externalLink: '',
     checkListItems: {
       incomeDetails: 'Income details',
       detailsOfAnySavings: 'Details of any savings',
@@ -30,14 +30,49 @@ export default {
   },
   createAccount: {
     title: 'Create account',
+    secondaryTitle: 'Create account',
     subtitle:
       'When you know you are eligible you can start your application on the Government website.',
     externalButtonTitle: 'Create Universal Credit account',
-    externalLink: '',
     checkListItems: {
       anEmailAddress: 'An email address',
       bankAccountDetails: 'Bank account details',
       childcareCosts: 'Childcare costs',
+    },
+  },
+  verifyIdentity: {
+    title: 'Verify Identity',
+    secondaryTitle: 'Information',
+    subtitle: 'As part of the process, you must verify your identity.',
+    externalButtonTitle: 'GOV UK Verify',
+    checkListItems: {
+      item1: 'Item 1',
+      item2: 'Item 2',
+      item3: 'Item 3',
+    },
+  },
+  apply: {
+    title: 'Apply',
+    secondaryTitle: 'Universal Credit Application',
+    subtitle:
+      'When you have created your account you can complete the main part of the application form.',
+    externalButtonTitle: 'Online Application Form',
+    checkListItems: {
+      item1: 'Item 1',
+      item2: 'Item 2',
+      item3: 'Item 3',
+    },
+  },
+  check: {
+    title: 'Check',
+    secondaryTitle:
+      'This may be an interview on the telephone or face to face.',
+    subtitle:
+      'After you have completed your application you will receive an appointment for an interview.',
+    checkListItems: {
+      item1: 'Item 1',
+      item2: 'Item 2',
+      item3: 'Item 3',
     },
   },
 };
--- client/src/context/lang.js
+++ client/src/context/lang.js
@@ -1,14 +1,41 @@
 import { createContext, useState, useContext } from 'react';
 
+const langOptions = [
+  { value: 'english', label: 'English' },
+  { value: 'french', label: 'French' },
+];
+
+const initialLang = 'english';
+
+const storeLangIntoStorage = (lang) => {
+  localStorage.setItem('lang', JSON.stringify(lang));
+};
+
+const getLangFromStorage = () => {
+  const lang = JSON.parse(localStorage.getItem('lang'));
+  if (lang) {
+    return lang;
+  } else {
+    storeLangIntoStorage(initialLang);
+    return initialLang;
+  }
+};
+
 const LangContext = createContext({
-  state: 'english',
+  lang: 'english',
+  langOptions: [],
   setLang: () => {},
 });
 
 const LangProvider = ({ children, ...props }) => {
-  const [lang, setLang] = useState('english');
+  const [lang, _setLang] = useState(getLangFromStorage);
 
-  const value = { lang, setLang };
+  const setLang = (newLang) => {
+    _setLang(newLang);
+    storeLangIntoStorage(newLang);
+  };
+
+  const value = { lang, langOptions, setLang };
   return (
     <LangContext.Provider value={value} {...props}>
       {children}
