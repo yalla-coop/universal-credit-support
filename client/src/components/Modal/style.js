158ab9a6df4af795eb4cfa0732e0099970e50d55
fadi <fadeomar2015@gmail.com>
2021-07-19 15:00:36 +0300

    update the style fo the modal

--- client/src/components/Modal/style.js
+++ client/src/components/Modal/style.js
@@ -7,20 +7,22 @@ export const Background = styled.div`
 
   position: absolute;
   width: 100vw;
+  min-height: 100%;
   height: 100vh;
   top: 0;
   left: 0;
-
   z-index: 10;
   background: ${({ theme }) => theme.colors.modalBackground};
 `;
 
 export const Container = styled.div`
-  min-width: 730px;
+  width: 730px;
   min-height: 532px;
+
   padding: ${({ theme }) => theme.spacings[6]};
   background: ${({ theme }) => theme.colors.white};
   ${({ theme }) => theme.media.tablet} {
+    width: 100vw;
     min-height: 100vh;
     min-width: 100vw;
   }
