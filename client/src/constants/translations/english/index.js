9e55567e3b4e6c9441b1b4c94e95984b0217fbc1
fadi <fadeomar2015@gmail.com>
2021-07-19 14:59:59 +0300

    update text content on the English

--- client/src/constants/translations/english/index.js
+++ client/src/constants/translations/english/index.js
@@ -18,61 +18,111 @@ export default {
   // STEPS
   checkEligibility: {
     title: 'Check Eligibility',
-    secondaryTitle: 'Check Eligibility',
+    secondaryTitle: 'Check eligibility (optional)',
     subtitle:
-      'You can check your eligibility to claim Universal Credit with an eligibility calculator.',
-    externalButtonTitle: 'Eligibility calculator',
+      'Using the benefit calculator enables you to find out an estimate of how much Universal Credit you may be entitled to.',
+    externalButtonTitle: 'Eligibility Calculator',
     checkListItems: {
       incomeDetails: 'Income details',
       detailsOfAnySavings: 'Details of any savings',
       housingCosts: 'Housing costs',
+      childcareCosts: 'Childcare costs',
+      incomeOthersDetails:
+        'Income details of any other adults living in the property that are not lodgers e.g. grown up children  elderly parents',
     },
   },
+
   createAccount: {
     title: 'Create account',
     secondaryTitle: 'Create account',
     subtitle:
-      'When you know you are eligible you can start your application on the Government website.',
-    externalButtonTitle: 'Create Universal Credit account',
+      'When you know you are eligible create your account as soon as possible on the Government website.',
+    externalButtonTitle: 'Create account',
     checkListItems: {
-      anEmailAddress: 'An email address',
-      bankAccountDetails: 'Bank account details',
-      childcareCosts: 'Childcare costs',
+      anEmailAddress: 'Email address',
+      bankAccountDetails: 'Bank account',
+      accessPhone: 'Access to your mobile phone (if you have one)',
+      partnerLinkingCode:
+        "Your partner's Universal Credit linking code (if you have a partner and they have one)",
+    },
+  },
+  claim: {
+    title: 'Make your claim',
+    secondaryTitle: 'Make your claim',
+    subtitle:
+      "Now it's time to complete the main part of the application form. Let's do this!",
+    externalButtonTitle: 'Apply here',
+    checkListItems: {
+      AccountInfo:
+        'Account log in information (that you created in the previous step)',
+      mobileAccess: 'Access to the mobile phone stored for your account',
+      bankAccount: 'A bank account',
+      homeDetails:
+        'Details of your home e.g. the information on your tenancy agreement',
+      housingCosts: 'Housing costs',
+      childrenBenefit: 'Benefit reference numbers for your children',
+      capitalDetails:
+        'Details of any capital and savings - includes money in your current account',
+      responsibilities:
+        'Details of caring responsibilities for a disabled person (if providing over 35 hours per week)',
+      bankAccountDetails:
+        'Your bank account details where UC will be paid into',
+      healthDetails:
+        'Details of any health issues that affect your ability to work',
     },
   },
   verifyIdentity: {
     title: 'Verify Identity',
-    secondaryTitle: 'Information',
-    subtitle: 'As part of the process, you must verify your identity.',
-    externalButtonTitle: 'GOV UK Verify',
-    checkListItems: {
-      item1: 'Item 1',
-      item2: 'Item 2',
-      item3: 'Item 3',
-    },
-  },
-  apply: {
-    title: 'Apply',
-    secondaryTitle: 'Universal Credit Application',
+    secondaryTitle: 'Verify identity',
     subtitle:
-      'When you have created your account you can complete the main part of the application form.',
-    externalButtonTitle: 'Online Application Form',
+      'Before your claim can be considered, you will need to ‘verify’ your identity. The online system will automatically direct you to Gov UK Verify.',
+    // externalButtonTitle: 'GOV UK Verify',
     checkListItems: {
-      item1: 'Item 1',
-      item2: 'Item 2',
-      item3: 'Item 3',
+      ID: 'ID (e.g. Passport / Driving Licence / Debit or Credit card)',
+      details:
+        'Details of any credit agreements e.g. dates you took out your mobile phone contract or a credit card',
     },
   },
   check: {
     title: 'Check',
-    secondaryTitle:
-      'This may be an interview on the telephone or face to face.',
+    secondaryTitle: 'Attend Interview',
     subtitle:
-      'After you have completed your application you will receive an appointment for an interview.',
+      "This might be in person or by phone. Phone the number above to arrange your interview. If you haven't been able to verify your identity online you might need to have an extra interview just to sort that out first",
+    externalButtonTitle: 'Call 0800 328 5644',
+
     checkListItems: {
-      item1: 'Item 1',
-      item2: 'Item 2',
-      item3: 'Item 3',
+      nationalNumber: 'National Insurance number',
+      needs: 'When arranging an interview, ask them what you need to bring',
+      limitations:
+        "Make sure you're ready to tell them about any limitations you have on getting work",
+    },
+  },
+  apply: {
+    title: 'Apply',
+    secondaryTitle: 'Apply for an advance (optional)',
+    subtitle:
+      "Don't be scared about applying for this if you definitely do need it! But make sure you go in with your eyes open. Taking an Advance is optional and it is a loan and it will be taken from your UC at an amount set by the DWP.",
+    externalButtonTitle: 'Apply for advance',
+    checkListItems: {
+      accountInfo: 'Account log in information (created in the Apply step)',
+      phone: 'Mobile phone',
+      details:
+        'Details of your job centre where you have had your interview and identity checked',
+    },
+  },
+  payment: {
+    title: 'First Payment',
+    secondaryTitle: 'Getting your first payment',
+    subtitle:
+      "First payment  is made 1 month and 7 days after you submitted your claim. Make sure you have done the things you need listed below or you won't get paid!",
+    externalButtonTitle: 'Check account',
+    checkListItems: {
+      accountInfo:
+        'Account log in information created in the set up account stage',
+      phone: 'Mobile phone you have registered with UC (if using one)',
+      email: 'Access to your email address that you have registered with UC',
+      toDoList: 'Completed all your to-do-list tasks',
+      claimantCommitment: 'Accepted your claimant commitment',
     },
   },
 };
