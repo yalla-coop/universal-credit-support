5441bde4c2608b40ef78b7b3b71f003e798a9c6b
fadi <fadeomar2015@gmail.com>
2021-07-19 14:58:42 +0300

    update the modal incase the options more than 5

--- client/src/pages/Step/index.js
+++ client/src/pages/Step/index.js
@@ -25,7 +25,7 @@ function Step() {
   const { steps, checkUncheckItem, setJustCompletedId } = useSteps();
 
   const step = steps.find((s) => s.id === params.id);
-
+  const desktopColWidth = step.checkListItems.length > 5 ? 6 : 12;
   return (
     <Modal>
       <S.Container>
@@ -42,35 +42,49 @@ function Step() {
           </Col>
         </Row>
         <Row style={{ flex: 1 }}>
-          <Col w={[4, 12, 6]}>
-            <div style={{ width: '100%' }}>
-              {step.externalButtonLink && (
-                <Button
-                  variant="primary"
-                  text={t(`${step.name}.externalButtonTitle`, lang)}
-                  to={n.EXTERNAL[step.externalButtonLink]}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  external
-                />
-              )}
-              <T.P isSmall weight="bold" mt="7" mb="5">
-                {t('informationYouWillNeed', lang)}
-              </T.P>
+          <div style={{ width: '100%' }}>
+            {step.externalButtonLink && (
+              <Row inner>
+                <Col w={[4, 12, 6]}>
+                  <Button
+                    variant="primary"
+                    text={t(`${step.name}.externalButtonTitle`, lang)}
+                    to={n.EXTERNAL[step.externalButtonLink]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    external
+                  />
+                </Col>
+              </Row>
+            )}
+            <T.P isSmall weight="bold" mt="7" mb="5">
+              {t('informationYouWillNeed', lang)}
+            </T.P>
+            <Row inner>
               {step.checkListItems.map((item) => (
-                <Checkbox
-                  key={item.value}
-                  checked={item.isChecked}
-                  handleChange={() => checkUncheckItem(step.name, item.value)}
-                  label={t(`${step.name}.checkListItems.${item.value}`, lang)}
-                  mb="5"
-                />
+                <Col w={[4, 12, desktopColWidth]}>
+                  <Checkbox
+                    key={item.value}
+                    checked={item.isChecked}
+                    handleChange={() => checkUncheckItem(step.name, item.value)}
+                    label={t(`${step.name}.checkListItems.${item.value}`, lang)}
+                    mb="5"
+                  />
+                </Col>
               ))}
-            </div>
-            <div
-              style={{ width: '100%', height: '58px', alignSelf: 'flex-end' }}
-            >
-              {step.isCompleted && (
+            </Row>
+          </div>
+
+          <Row
+            inner
+            style={{
+              width: '100%',
+              minHeight: '58px',
+              alignSelf: 'flex-end',
+            }}
+          >
+            {step.isCompleted ? (
+              <Col w={[4, 12, 6]}>
                 <Button
                   variant="secondary"
                   text={t('nextStep', lang)}
@@ -79,17 +93,20 @@ function Step() {
                     setJustCompletedId(step.id);
                   }}
                 />
-              )}
-              {!step.isCompleted && step.externalLink && (
-                <CallUsLink
-                  text={t('callUsLinkText', lang)}
-                  href={n.EXTERNAL.CALL_US}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                />
-              )}
-            </div>
-          </Col>
+              </Col>
+            ) : (
+              step.externalLink && (
+                <Col w={[4, 12, 6]}>
+                  <CallUsLink
+                    text={t('callUsLinkText', lang)}
+                    href={n.EXTERNAL.CALL_US}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  />
+                </Col>
+              )
+            )}
+          </Row>
         </Row>
       </S.Container>
     </Modal>
