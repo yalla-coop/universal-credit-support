6311b6e47437755a1990a1f2d14dfde62f80a463
fadi <fadeomar2015@gmail.com>
2021-08-17 17:44:30 +0300

    create helpButton component

---
+++ client/src/components/HelpButton/index.js
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const HelpButton = () => {
+  return <S.Button>help</S.Button>;
+};
+
+export default HelpButton;
---
+++ client/src/components/HelpButton/style.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  width: 75px;
+  height: 45px;
+  background: red;
+  border-radius: 12px 12px 0px 0px;
+  transform: rotate(-90deg);
+  position: absolute;
+  right: 0;
+  top: 500px;
+  padding: 0;
+  margin: 0;
+  border: 0;
+`;
