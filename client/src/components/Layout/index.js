1888426ec7b44b085fcc4d0516b8a0308c3bfcea
fadi <fadeomar2015@gmail.com>
2021-08-19 12:17:46 +0300

    create dashboard layout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 
 import General from './General';
 import SplitScreen from './SplitScreen';
+import Dashboard from './Dashboard';
 
 const Layout = ({ layout, ...props }) => {
   switch (layout) {
+    case 'dashboard':
+      return <Dashboard {...props} />;
     case 'splitScreen':
       return <SplitScreen {...props} />;
     case 'step':
