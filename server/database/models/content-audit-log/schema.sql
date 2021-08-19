44f1507af74d54b5f94c4e6afcfc8c605f5fcb44
Ahmed Shatat <a.shatat@hotmail.com>
2021-08-19 13:26:28 +0300

    fix config url
    add step_id to audit logs
    
    #56

--- server/database/models/content-audit-log/schema.sql
+++ server/database/models/content-audit-log/schema.sql
@@ -8,6 +8,8 @@ DROP TABLE IF EXISTS "content_audit_log" CASCADE;
 CREATE TABLE "content_audit_log" (
   "id" SERIAL PRIMARY KEY,
   "user_id" INTEGER REFERENCES users(id),
+  "step_id" INTEGER REFERENCES steps(id),
+  "landing_page_content_id" INTEGER REFERENCES landing_page_content(id),
   "type" content_audit_log_types NOT NULL,
   "updated_content" JSON,
     -- { 
