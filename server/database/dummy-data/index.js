b75443a2d549f3a3116a43d2813ddfa7e15722f4
thejoefriel <joseph.s.friel@gmail.com>
2021-08-20 22:04:00 +0100

    Build on dummy and production > #57

--- server/database/dummy-data/index.js
+++ server/database/dummy-data/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import createUsers from './users';
 import createOrganisations from './organisations';
 import createLandingPage from '../dummy-data-prod/landingPageContent';
+import createSteps from '../dummy-data-prod/stepsContent';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -11,7 +12,8 @@ const buildData = async () => {
   const createdData = {};
   createdData.users = await createUsers(createdData);
   createdData.organisations = await createOrganisations(createdData);
-  createdData.landingPage = await createLandingPage(createdData);
+  createdData.landingPage = await createLandingPage();
+  createdData.steps = await createSteps();
 
   return createdData;
 };
