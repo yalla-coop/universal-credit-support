8ec6ca68a41a7f16e69bb7e79901057e2457638c
Ahmed Shatat <a.shatat@hotmail.com>
2021-08-23 16:23:54 +0300

    update steps context logic
    #65

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,17 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 
 import Step from '../../components/Steps';
-import { Typography as T, Inputs as I } from '../../components';
+import { Typography as T } from '../../components';
 import { t } from '../../helpers';
 import { useLang } from '../../context/lang';
 import { useSteps } from '../../context/steps';
 import { navRoutes as n } from '../../constants';
+import LandingContent from './LandingContent';
 
 import Icon from '../../components/Icon';
 import HelpButton from '../../components/HelpButton';
 import TextWithIcon from '../../components/TextWithIcon';
-
-import { LandingPage } from '../../api-calls';
 
 import * as S from './style';
 
@@ -28,36 +27,20 @@ const afterClaimContent = {
 
 const Home = () => {
   const { lang } = useLang();
-  const { steps: fullSteps, justCompletedId, setJustCompletedId } = useSteps();
-  const [landingContent, setLandingContent] = useState({});
-  const [steps, setSteps] = useState({
-    beforeClaiming: [],
-    claiming: [],
-    afterClaiming: [],
-  });
-  const [showAfterClaim, setShowAfterClaim] = useState(false);
-  const [fetchError, setFetchError] = useState('');
+  const {
+    steps,
+    justCompletedId,
+    setJustCompletedId,
+    loadingSteps,
+    stepsObj,
+  } = useSteps();
 
-  const currentStep = fullSteps.find((step) => !step.isCompleted);
+  const [showAfterClaim, setShowAfterClaim] = useState(false);
+
+  const currentStep = steps.find((step) => !step.isCompleted);
   const currentStepRef = useRef();
 
   const completedClaim = currentStep?.stage === 'afterClaiming';
-
-  const formatText = (text) => {
-    if (!text) return '';
-    const arr = text.split(/\. |\! |\? /gm);
-    const firstSentence = arr[0];
-    if (!arr[1]) return <T.H2 color="primaryMain">{firstSentence}</T.H2>;
-
-    const remainder = arr.slice(1).join(' ');
-    return (
-      <>
-        <T.H2 color="primaryMain" mb="2" mr="2" mt="2">
-          {firstSentence}! <S.Span>{remainder}</S.Span>
-        </T.H2>
-      </>
-    );
-  };
 
   const getStepStatus = (step, i) => {
     const isCurrentStep = currentStep && step.name === currentStep.name;
@@ -85,53 +68,12 @@ const Home = () => {
     }
   }, [justCompletedId]);
 
-  useEffect(() => {
-    let mounted = true;
-    async function fetchData() {
-      const { data, error } = await LandingPage.getLandingPageContent({});
-      if (mounted) {
-        if (error) {
-          setFetchError(error.message);
-        } else {
-          setLandingContent(data);
-        }
-      }
-    }
-
-    fetchData();
-
-    const stepsObj = fullSteps.reduce((acc, curr) => {
-      const { stage } = curr;
-      if (!acc[stage]) {
-        acc[stage] = [];
-      }
-      acc[stage].push(curr);
-      return acc;
-    }, {});
-    if (mounted) {
-      setSteps(stepsObj);
-    }
-    return () => {
-      mounted = false;
-    };
-  }, []);
-
   return (
     <>
-      <S.PageHead>
-        <S.HeaderText>
-          <T.H2 weight="bold" color="white">
-            {landingContent.headline}
-          </T.H2>
-        </S.HeaderText>
-      </S.PageHead>
-      <S.Section>
-        {formatText(landingContent.subtitle)}{' '}
-        <S.StyledText mb="3">{landingContent.instructions}</S.StyledText>
-      </S.Section>
+      <LandingContent />
 
       {/* BEFORE CLAIMING */}
-      {steps.beforeClaiming?.map((step, i) => {
+      {stepsObj.BEFORE_CLAIMING?.map((step, i) => {
         const { variant, currentRef, isJustCompletedOne } = getStepStatus(
           step,
           i
@@ -154,12 +96,13 @@ const Home = () => {
             handleClick={() => {
               setJustCompletedId('');
             }}
+            loadingSteps={loadingSteps}
           />
         );
       })}
 
       {/* CLAIMING */}
-      {steps.claiming?.map((step, i) => {
+      {stepsObj.CLAIMING?.map((step, i) => {
         const { variant, currentRef, isJustCompletedOne } = getStepStatus(
           step,
           i
@@ -182,6 +125,7 @@ const Home = () => {
             handleClick={() => {
               setJustCompletedId('');
             }}
+            loadingSteps={loadingSteps}
           />
         );
       })}
@@ -219,7 +163,7 @@ const Home = () => {
       </S.Section>
 
       {(completedClaim || showAfterClaim) &&
-        steps.afterClaiming?.map((step, i) => {
+        stepsObj.AFTER_CLAIMING?.map((step, i) => {
           const { variant, currentRef, isJustCompletedOne } = getStepStatus(
             step,
             i
@@ -242,6 +186,7 @@ const Home = () => {
               handleClick={() => {
                 setJustCompletedId('');
               }}
+              loadingSteps={loadingSteps}
             />
           );
         })}
