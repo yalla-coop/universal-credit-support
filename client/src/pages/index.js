247442ba162baf87cda0e1bd7699088f1d3de8cb
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2021-08-24 23:43:54 +0300

    chore: export forget & reset password pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,7 @@ import Home from './Home';
 import Step from './Step';
 import CreateUniqueLink from './CreateUniqueLink';
 import Login from './Login';
+import ForgotPassword from './ForgotPassword';
+import ResetPassword from './ResetPassword';
 
-export { Home, Step, Login, CreateUniqueLink };
+export { Home, Step, Login, CreateUniqueLink, ForgotPassword, ResetPassword };
