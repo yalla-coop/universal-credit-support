2580bfcae82d35510436ea8cf203f51f1ceee30a
fadi <fadeomar2015@gmail.com>
2021-08-29 19:15:20 +0300

    fix changes

--- client/src/pages/Changes/index.js
+++ client/src/pages/Changes/index.js
@@ -29,7 +29,7 @@ const Changes = () => {
           <T.H1 mtM="5" style={{ width: '100%' }}>
             Content changes
           </T.H1>
-          <T.P mt="7" mtM="4">
+          <T.P mt="7" mtM="4" color="neutralDark">
             Here is a log of any changes made to the core content of the tool.
           </T.P>
         </Col>
@@ -40,14 +40,17 @@ const Changes = () => {
             {pageError}
           </T.P>
         )}
-        {changes &&
+        {changes && changes.length ? (
           changes.map((change) => (
-            <Col w={[4, 4, 4]} mt="6">
+            <Col w={[4, 12, 5]} mt="6">
               <S.ContentWrapper>
                 <T.P weight="bold" mr="2">
                   Date:
                 </T.P>
-                <T.P> {moment(change.updatedAt).format('DD/MM/YYYY')}</T.P>
+                <T.P color="neutralDark">
+                  {' '}
+                  {moment(change.updatedAt).format('DD/MM/YYYY')}
+                </T.P>
               </S.ContentWrapper>
               <S.ContentWrapper>
                 <T.P weight="bold" mr="2">
@@ -58,7 +61,10 @@ const Changes = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <T.P color="secondaryMain"> {change.email}</T.P>
+                  <T.P color="neutralDark" underline>
+                    {' '}
+                    {change.email}
+                  </T.P>
                 </a>
               </S.ContentWrapper>
               {change.landingPageContentId ? (
@@ -84,10 +90,16 @@ const Changes = () => {
                 </>
               )}
             </Col>
-          ))}
+          ))
+        ) : (
+          <Row mt="5">
+            <Col w={(4, 12, 12)}>
+              <T.P weight="semi">There is no changes to show </T.P>
+            </Col>
+          </Row>
+        )}
       </Row>
     </>
   );
 };
-
 export default Changes;
