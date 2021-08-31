0aab7cc17dc683369ead308c476e8e87ed3b386d
RamyAlshurafa <ramyshurafa@hotmail.com>
2021-08-31 14:54:05 +0300

    disable csp

--- server/api/middlewares/helmet.js
+++ server/api/middlewares/helmet.js
@@ -3,40 +3,41 @@ import helmet from 'helmet';
 
 // const { bucket, bucketRegion } = config.aws;
 const helmetMiddleware = helmet({
-  contentSecurityPolicy: {
-    useDefaults: true,
-    reportOnly: false,
-    directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      // fallback for other directives
-      // allowed urls to make requests to
-      'default-src': [
-        "'self'",
-        // 'https://www.google-analytics.com/',
-        // 'https://consent.cookiebot.com/',
-        // 'https://o557886.ingest.sentry.io/',
-        // 'https://www.google.com/recaptcha/',
-        // 'https://consentcdn.cookiebot.com/',
-        // 'https://www.gstatic.com/recaptcha/',
-        // `https://${bucket}.s3.${bucketRegion}.amazonaws.com/`,
-      ],
-      // trusted <scripts>
-      'script-src': [
-        "'self'",
-        // 'https://www.google-analytics.com/',
-        // 'https://consent.cookiebot.com/',
-        // 'https://consentcdn.cookiebot.com/',
-        // 'https://o557886.ingest.sentry.io/',
-        // 'https://www.google.com/recaptcha/',
-        // 'https://www.gstatic.com/recaptcha/',
-      ],
-      'img-src': [
-        "'self'",
-        'data:',
-        // `https://${bucket}.s3.${bucketRegion}.amazonaws.com/`,
-      ],
-    },
-  },
+  // contentSecurityPolicy: {
+  //   useDefaults: true,
+  //   reportOnly: false,
+  //   directives: {
+  //     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+  //     // fallback for other directives
+  //     // allowed urls to make requests to
+  //     'default-src': [
+  //       "'self'",
+  //       // 'https://www.google-analytics.com/',
+  //       // 'https://consent.cookiebot.com/',
+  //       // 'https://o557886.ingest.sentry.io/',
+  //       // 'https://www.google.com/recaptcha/',
+  //       // 'https://consentcdn.cookiebot.com/',
+  //       // 'https://www.gstatic.com/recaptcha/',
+  //       // `https://${bucket}.s3.${bucketRegion}.amazonaws.com/`,
+  //     ],
+  //     // trusted <scripts>
+  //     'script-src': [
+  //       "'self'",
+  //       // 'https://www.google-analytics.com/',
+  //       // 'https://consent.cookiebot.com/',
+  //       // 'https://consentcdn.cookiebot.com/',
+  //       // 'https://o557886.ingest.sentry.io/',
+  //       // 'https://www.google.com/recaptcha/',
+  //       // 'https://www.gstatic.com/recaptcha/',
+  //     ],
+  //     'img-src': [
+  //       "'self'",
+  //       'data:',
+  //       // `https://${bucket}.s3.${bucketRegion}.amazonaws.com/`,
+  //     ],
+  //   },
+  // },
+  contentSecurityPolicy: false,
   dnsPrefetchControl: { allow: true },
   referrerPolicy: { policy: 'strict-origin' },
 });
