1012f9a5d52eb43d53bb2cbdbf1c1982cf161352
RamyAlshurafa <ramyshurafa@hotmail.com>
2021-09-01 20:39:19 +0300

    replace replaceAll with replace

--- server/helpers/create-org-slug.js
+++ server/helpers/create-org-slug.js
@@ -1,5 +1,5 @@
 const createOrgSlug = (orgName) => {
-  const slug = orgName.trim().toLowerCase().replaceAll(' ', '-');
+  const slug = orgName.trim().toLowerCase().replace(/ /g, '-');
   return slug;
 };
 
