14be098bd32039fe795be151930c04f364f7f814
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2021-09-16 16:17:46 +0300

    Update client/src/helpers/update-gradients.js

--- client/src/helpers/update-gradients.js
+++ client/src/helpers/update-gradients.js
@@ -1,5 +1,4 @@
 const updateGradients = (updatedColors) => {
-  console.log('UPDA', updatedColors);
   return {
     primary: `linear-gradient(90deg, ${updatedColors.primaryMain} 0%, ${updatedColors.primaryMid} 98.89%)`,
     secondary: `linear-gradient(90deg, ${updatedColors.secondaryMain} 0%, ${updatedColors.secondaryMid} 98.89%)`,
