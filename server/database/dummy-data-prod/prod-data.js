74bc13f12ecb132415ada0c77a0cc9c4de65df92
RamyAlshurafa <ramyshurafa@hotmail.com>
2022-10-19 09:30:10 +0300

    remove personal data

--- server/database/dummy-data-prod/prod-data.js
+++ server/database/dummy-data-prod/prod-data.js
@@ -1,12 +1,15 @@
-// 123456Aa password
-const password = '$2a$08$23ik.euo.8EM.tqkX/43ke539bnaWX/2vK8nsbrdlYl0UhGMwCR92';
+const generateRandomPassword = () => {
+  const buffer = crypto.randomBytes(32);
+  const password = buffer.toString('hex');
+  return password;
+};
 
 export default {
-  password,
+  password: generateRandomPassword(),
   hydeSuperAdmin: {
     firstName: 'Joe',
     lastName: 'Friel',
-    email: 'superadmin@hyde.co.uk',
-    backupEmail: 'joe@yallacooperative.com',
+    email: process.env.HYDE_SUPER_ADMIN_EMAIL,
+    backupEmail: process.env.HYDE_SUPER_ADMIN_EMAIL_BACKUP,
   },
 };
