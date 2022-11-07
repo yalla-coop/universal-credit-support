37ff32c4fad2ff90821092a1d793a746f895aef6
fadi <fadeomar2015@gmail.com>
2022-11-07 12:30:57 +0200

    fix packages

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,15 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "storybook-addon-emotion-theme/dist/register"],
-  core: {
-    builder: "webpack5"
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
   }
-};
+}
