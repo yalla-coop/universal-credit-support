78cc8c5e345897c2985977d51e8bac169fdeecfb
cemalokten <cemalokten@gmail.com>
2022-11-09 18:59:40 +0000

    Add 12 language display limit mobile

--- client/src/components/LanguageSelector/index.js
+++ client/src/components/LanguageSelector/index.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { TextWithIcon, Icon } from '../../components';
 import { Row } from '../Grid';
+import { Grid } from 'antd';
 import { FlagMap } from '../../components/Icon';
 import { BasicInput } from '../Inputs/index';
 import * as S from './style';
+const { useBreakpoint } = Grid;
 
 const props = {
   weight: 'medium',
@@ -16,10 +18,28 @@ const languageCodes = {
   en: 'English',
   es: 'Spanish',
   bm: 'Bambara',
+  as: 'French',
+  ts: 'Turkish',
+  fs: 'Welsh',
+  gs: 'Swedish',
+  vs: 'Japanese',
+  am: 'Bambara',
+  qm: 'German',
+  am: 'Russian',
+  lm: 'Italian',
+  ii: 'Hide on mobile',
 };
 
 export const LanguageSelector = () => {
   const [search, setSearch] = useState('');
+
+  const screens = useBreakpoint();
+  const sliceTo = Object.entries(screens)
+    .filter((screen) => !!screen[1])
+    .map((screen) => screen[0])
+    .includes('sm')
+    ? languageCodes.length
+    : 12;
 
   const languages = Object.entries(languageCodes).filter(([code, language]) => {
     return (
@@ -41,18 +61,20 @@ export const LanguageSelector = () => {
         />
       </S.ButtonWrapper>
       <S.ButtonWrapper>
-        {languages.map(([code, language]) => {
-          return (
-            <S.Button>
-              <TextWithIcon
-                handleClick={() => null}
-                text={language}
-                icon={FlagMap[code] !== undefined ? code : null}
-                {...props}
-              />
-            </S.Button>
-          );
-        })}
+        {languages
+          .map(([code, language]) => {
+            return (
+              <S.Button>
+                <TextWithIcon
+                  handleClick={() => null}
+                  text={language}
+                  icon={FlagMap[code] !== undefined ? code : null}
+                  {...props}
+                />
+              </S.Button>
+            );
+          })
+          .slice(0, sliceTo)}
       </S.ButtonWrapper>
     </Row>
   );
