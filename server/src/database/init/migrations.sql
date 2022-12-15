a1114292844ebdd422a4fb304e2447c4083edc82
RamyAlshurafa <ramyshurafa@hotmail.com>
2022-12-15 13:54:30 +0200

    add migration files to convert to jsonb and fix build script

--- server/src/database/init/migrations.sql
+++ server/src/database/init/migrations.sql
@@ -11,4 +11,5 @@ INSERT INTO "migrations" ("name") VALUES
   ('/20221123121141-add-org-status'),
   ('/20221124140906-remove-backup-email-unique-constraint'),
   ('/20221201042815-add-deleted-status-for-organisations'),
-  ('/20221214090033-add-languages-tables');
+  ('/20221214090033-add-languages-tables'),
+  ('/20221214173721-convert-json-to-jsonb');
