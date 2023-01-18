77957b1c0b01d68c18723c5c84c240529c8e5fa1
cemalokten <cemalokten@gmail.com>
2023-01-18 12:00:00 +0000

    Add boolean ternary forPublic

--- server/src/modules/landing-page/controllers/get-landing-page.js
+++ server/src/modules/landing-page/controllers/get-landing-page.js
@@ -3,9 +3,10 @@ import * as landingPage from '../use-cases';
 const getLandingPage = async (req, res, next) => {
   try {
     const { lng, forPublic } = req.query;
+    const _forPublic = forPublic === 'true' ? true : false
     const landingPageContent = await landingPage.getLandingPage({
       lng,
-      forPublic,
+      forPublic : _forPublic,
     });
 
     res.json(landingPageContent);
