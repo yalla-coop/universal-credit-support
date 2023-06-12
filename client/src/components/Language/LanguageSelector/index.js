3a6478cfe53c73ccc569e451b328746856af6719
thejoefriel <joseph.s.friel@gmail.com>
2023-06-12 14:22:22 +0100

    Add language tracker

--- client/src/components/Language/LanguageSelector/index.js
+++ client/src/components/Language/LanguageSelector/index.js
@@ -7,6 +7,7 @@ import { useMediaQuery } from 'react-responsive';
 import theme from '../../../theme';
 import { useTranslation } from 'react-i18next';
 import { types, common } from '../../../constants';
+import ReactGA from 'react-ga4';
 
 const props = {
   weight: 'medium',
@@ -36,6 +37,14 @@ export const LanguageSelector = ({ hide, handleHide }) => {
 
   const changeLanguage = ({ lng }) => {
     i18n.changeLanguage(types.languageCodes[lng]);
+
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.event({
+        category: 'Language changed',
+        action: types.languageCodes[lng],
+      });
+    }
+
     handleHide();
   };
 
